1e74708bb0d16ea7f5e9f35a9e642e4de4644cdb
randolphvip <shangcongvip@163.com>
2020-04-22 15:46:51 +1200

    update

---
+++ src/util/Content.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Content {
+	//	public final String userSession="user";
+		
+		
+		public static final String USER_SESSION="user";
+		
+		public static final String MESSAGE="#MESSAGE";
+}
